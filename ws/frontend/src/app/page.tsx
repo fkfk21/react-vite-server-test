2f5831a1a170481625440ba5cb1c1f5b02b2d7fc
Kenji Fukuda <fk.kenji.21@gmail.com>
2024-03-10 18:18:29 +0900

    add react-router-dom

---
+++ ws/frontend/src/app/page.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useRef, useState } from 'react';
+import reactLogo from '@/assets/react.svg';
+import viteLogo from '/vite.svg';
+
+import './styles.css';
+
+export function Page() {
+  const [count, setCount] = useState(0);
+
+  const ws = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    if (!ws.current) {
+      ws.current = new WebSocket('ws://localhost:5000');
+      ws.current.onopen = () => {
+        console.log('connected');
+      };
+      ws.current.onmessage = (event) => {
+        console.log('Received: ' + event.data);
+      };
+      ws.current.onclose = () => {
+        console.log('disconnected');
+      };
+      ws.current.onerror = (event) => {
+        console.error('WebSocket error observed:', event);
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button
+          onClick={() => {
+            ws.current!.send('Hello, world!');
+            setCount((count) => count + 1);
+          }}
+        >
+          count is {count}
+        </button>
+        <button
+          onClick={() => {
+            fetch('/api/hello')
+              .then((res) => res.json())
+              .then(console.log);
+          }}
+        >
+          fetch
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default Page;
---
+++ ws/frontend/src/app/test/page.tsx
@@ -0,0 +1,8 @@
+const Page = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+export default Page;
